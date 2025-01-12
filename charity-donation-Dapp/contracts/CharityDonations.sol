d6f08085adeeb4154bf540aeda2a0372f10809c2
lolosaisa <lwamaitha3924@gmail.com>
2025-01-12 12:30:54 +0300

    added th event's necessary to enhance transparency

--- charity-donation-Dapp/contracts/CharityDonations.sol
+++ charity-donation-Dapp/contracts/CharityDonations.sol
@@ -27,11 +27,17 @@ contract Donation {
     }
 
     //storage - mappings and arrays
+    uint public campaignCount;
+
     mapping(uint => Campaign) public campaigns;
     mapping(uint => Donor[]) public donors;
     mapping(address => Donations[]) public donations;
     mapping(uint => address[]) public topContributors;
     mapping(uint256 => mapping(address => bool)) public hasDonated;
+
+// setting a minimum donation amount
+    uint public constant MIN_DONATION = 0.01 ether;
+
 
     //events
     event CampaignCreated(
@@ -41,7 +47,21 @@ contract Donation {
         uint goal,
         uint deadline,
         address owner
+    );
+    event DonationReceived(
+        uint campaignId,
+        uint amount,
+        address donor,
+        uint totalAmountRaised
+    );
+    event CampaignCompleted(
+        uint campaignId,
+        uint totalAmountRaised,
+        uint numberOfDonors,
     )
+
+    
+
         
 
 }
