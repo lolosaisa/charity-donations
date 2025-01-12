87c5d5fb8e82af960475ddcd9947176d423e4107
lolosaisa <lwamaitha3924@gmail.com>
2025-01-12 12:24:47 +0300

    added the the storage/ mapping and arrays needed

--- charity-donation-Dapp/contracts/CharityDonations.sol
+++ charity-donation-Dapp/contracts/CharityDonations.sol
@@ -26,4 +26,22 @@ contract Donation {
         address donor;
     }
 
+    //storage - mappings and arrays
+    mapping(uint => Campaign) public campaigns;
+    mapping(uint => Donor[]) public donors;
+    mapping(address => Donations[]) public donations;
+    mapping(uint => address[]) public topContributors;
+    mapping(uint256 => mapping(address => bool)) public hasDonated;
+
+    //events
+    event CampaignCreated(
+        string title,
+        string description,
+        uint id,
+        uint goal,
+        uint deadline,
+        address owner
+    )
+        
+
 }
